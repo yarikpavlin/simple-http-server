8cb032c4205908868ad93231dcdf8ec88fe84f3b
yarikpavlin <yarbezl@gmail.com>
2022-02-06 21:34:15 +0200

    Add logging interceptor and simple routing

--- server.js
+++ server.js
@@ -3,8 +3,25 @@ const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST || '127.0.0.1';
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end('End of the response.');
+    switch(req.url){
+        case '/sayhello':
+            res.writeHead(200);
+            res.end('Hello World!');
+            break;
+        case '/geterror':
+            res.writeHead(500);
+            res.end('Ooops, error on server side..');
+            break;
+        default:
+            res.writeHead(200);
+            res.end(`I just dumb server. Try /sayhello or /geterror routes`);
+            break;
+    }
+});
+
+// Logging Interceptor
+server.on('request', (req, res) => {
+    console.log(`${req.method} ${req.url} ${res.statusCode} ${res.statusMessage}`);
 });
 
 server.listen(PORT, HOST, () => {
