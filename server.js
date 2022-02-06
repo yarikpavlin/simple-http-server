57421b3e06a4004d00f1e42b9a84ddd291b56d6c
yarikpavlin <yarbezl@gmail.com>
2022-02-06 21:48:12 +0200

    Update default route

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST || '127.0.0.1';
 
 const server = http.createServer((req, res) => {
-    switch(req.url){
+    switch(req.url) {
+        case '/':
+            res.writeHead(200);
+            res.end('Only two routes are available: /sayhello & /geterror');
         case '/sayhello':
             res.writeHead(200);
             res.end('Hello World!');
@@ -13,8 +16,8 @@ const server = http.createServer((req, res) => {
             res.end('Ooops, error on server side..');
             break;
         default:
-            res.writeHead(200);
-            res.end(`I just dumb server. Try /sayhello or /geterror routes`);
+            res.writeHead(501);
+            res.end('No Content');
             break;
     }
 });
